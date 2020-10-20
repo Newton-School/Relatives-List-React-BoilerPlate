62a25d025ad60643cafc9ad8480f5c41effe168b
Raja Babu Dubey <45536846+rajadubey@users.noreply.github.com>
2020-10-20 19:10:37 +0530

    change-1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-function generateCodeFromObject(obj){
-    //return a code generated string
-   }
-   
-module.exports=generateCodeFromObject;
+function generateCodeFromObject(obj) {
+  //return a code generated string
+  relatives = ["Hitesh", "Sanya", "Nilu", "Snehil", "Nisha"];
 
+  return (
+    <>
+      <ol key="realtiveList">
+        {this.relatives.map((relative, index) => (
+          <li key={"relativeListItem" + (index + 1)}>{relative}</li>
+        ))}
+      </ol>
+    </>
+  );
+}
+
+module.exports = generateCodeFromObject;
 
 // ReactDOM.render(<App />, document.getElementById("root"));
